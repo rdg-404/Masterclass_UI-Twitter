393f40d8fd1c706e977051b632d636e50bc46b58
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-10 23:19:53 -0300

    components: children

--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -2,7 +2,7 @@ export function Tweet(props) {
   return (
     <div>
       <strong>{props.user}</strong>
-      <p>{props.content}</p>
+      <p>{props.children}</p>
       <button>Like</button>     
     </div>
   )
--- src/main.tsx
+++ src/main.tsx
@@ -7,9 +7,15 @@ import { Tweet } from './components/Tweet'
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <div>
-    <Tweet user="Rodrigo" content="hello"/>
-    <Tweet user="Diego" content="hi"/>
-    <Tweet user="Ana" content="whats'up"/>
+    <Tweet user="Rodrigo">
+      hello
+    </Tweet>
+    <Tweet user="Diego">
+      hi
+    </Tweet>
+    <Tweet user="Ana">
+      whats'up
+    </Tweet>
     </div>
   </React.StrictMode>,
 )

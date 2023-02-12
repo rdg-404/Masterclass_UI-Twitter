45fc00fa1e6a532a9f98ab19d0dd419a9859191d
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-11 23:18:03 -0300

    config global.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,21 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Tweet } from './components/Tweet'
 import './global.css'
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <div>
-    <Tweet user="Rodrigo" likes={3}>
-      hello
-    </Tweet>
-    <Tweet user="Diego">
-      hi
-    </Tweet>
-    <Tweet user="Ana">
-      whats'up
-    </Tweet>
-    </div>
+
   </React.StrictMode>,
 )

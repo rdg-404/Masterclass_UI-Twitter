e5e5fbef06e682cbe87e83232e7c6eec46b7f50d
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-13 00:05:28 -0300

    created sidebar and estilized

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Hash, House, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle } from 'phosphor-react'
 import './global.css'
 
 import twitterLogo from './assets/logo-twitter.svg'
+
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -11,7 +13,40 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
       <aside className="sidebar">
         <img className='logo' src={twitterLogo} alt="Logo twitter" />
 
-        <nav></nav>
+        <nav className='main-navigation'>
+        <a className='active' href="">
+          <House weight='fill'/>
+          Home
+        </a>
+        <a href="">
+          <Hash />
+          Explore
+        </a>
+        <a href="">
+          <Bell/>
+          Notifications
+        </a>
+        <a href="">
+          <Envelope/>
+          Messages
+        </a>
+        <a href="">
+          <BookmarkSimple/>
+          Bookmarks
+        </a>
+        <a href="">
+          <FileText/>
+          Lists
+        </a>
+        <a href="">
+          <User/>
+          Profile
+        </a>
+        <a href="">
+          <DotsThreeCircle/>
+          More
+        </a>
+        </nav>
 
         <button className='new-tweet'>Tweet</button>
       </aside>

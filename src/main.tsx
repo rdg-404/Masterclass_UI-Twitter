baa3c326fd3f907a7ccc91d97eb9e95841100be3
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-12 23:43:09 -0300

    importacao da logo e estilizacao do botao tweet

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,22 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './global.css'
 
+import twitterLogo from './assets/logo-twitter.svg'
+
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <div className="layout">
+      <aside className="sidebar">
+        <img className='logo' src={twitterLogo} alt="Logo twitter" />
 
+        <nav></nav>
+
+        <button className='new-tweet'>Tweet</button>
+      </aside>
+      <div className="content">
+        content
+      </div>
+    </div>
   </React.StrictMode>,
 )

c41a3dff2ffed13f0ac4c0533888722e927fe689
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-13 00:34:42 -0300

    created timeline top and estilized

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Hash, House, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle } from 'phosphor-react'
+import { Hash, House, Bell, Envelope, BookmarkSimple, FileText, User, DotsThreeCircle, Sparkle } from 'phosphor-react'
 import './global.css'
 
 import twitterLogo from './assets/logo-twitter.svg'
@@ -51,7 +51,21 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <button className='new-tweet'>Tweet</button>
       </aside>
       <div className="content">
-        content
+        <main className="timeline">
+          <div className="timeline-header">
+            Home
+            <Sparkle/>
+          </div>
+
+          <form className='new-tweet-form'>
+            <label htmlFor="tweet">
+              <img src="http://github.com/rdg-404.png" alt="Rodrigo Paiva" />
+              <textarea id="tweet" placeholder="What's happening?"/>
+            </label>
+
+            <button type='submit'>Tweet</button>
+          </form>
+        </main>
       </div>
     </div>
   </React.StrictMode>,

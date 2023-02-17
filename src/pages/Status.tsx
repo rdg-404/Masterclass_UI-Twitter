7d330770b6918537a564105a633bc0c239e7e88a
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-17 19:23:06 -0300

    cria tweet que esta sendo comentado

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -11,19 +11,31 @@ const Answers = [
 
 export function Status() {
   return (
+
+
+
     <main className="status">
         <Header title="Tweet"/>
-          
+
+
+
+          <Tweet content="    Lorem ipsum, dolor sit amet consectetur adipisicing elit. Beatae dignissimos maxime quae, id tempora et cum unde obcaecati commodi neque distinctio nisi blanditiis assumenda optio nulla ea excepturi, similique nemo."/>
+
+
+          <Separator/>
+
           <form className='answer-tweet-form'>
             <label htmlFor="tweet">
               <img src="http://github.com/rdg-404.png" alt="Rodrigo Paiva" />
               <textarea id="tweet" placeholder="Tweet your answer"/>
             </label>
 
+
+
             <button type='submit'>Answer</button>
           </form>
 
-        <Separator/>
+    
 
         {Answers.map(answer => {
           return <Tweet key={answer} content={answer}/>

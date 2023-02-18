16fd959148f183f823a36a00046d8196b5bf4bd2
rodrigopaivadev <rodrigopaiva0502@gmail.com>
2023-02-18 19:34:39 -0300

    useState para salvar as novas respostas de tweets

--- src/pages/Status.tsx
+++ src/pages/Status.tsx
@@ -1,3 +1,4 @@
+import { FormEvent, useState } from "react"
 import { Header } from "../components/Header"
 import { Separator } from "../components/Separator"
 import { Tweet } from "../components/Tweet"
@@ -5,17 +6,27 @@ import { Tweet } from "../components/Tweet"
 import './Status.css'
 
 
-const Answers = [
-  'Concordo...',
-  'Olha, faz sentido',
-  'Gostei do seu perfil!'
-]
+
 
 
 export function Status() {
+
+  const [newAnswer, setNewAnswer] = useState('') //para limpar a textarea
+  const [answers, setAnswers] = useState([
+    'wswdwdw',
+    'Olha, faz sentido',
+    'Gostei do seu perfil!'
+  ])
+
+  function createNewAnswer(event: FormEvent) {
+    event.preventDefault() //impede o envio do formulario
+
+    setAnswers([newAnswer, ...answers])
+    setNewAnswer('')
+    
+  }
+  
   return (
-
-
 
     <main className="status">
         <Header title="Tweet"/>
@@ -27,10 +38,17 @@ export function Status() {
 
           <Separator/>
 
-          <form className='answer-tweet-form'>
+          <form onSubmit={createNewAnswer} className='answer-tweet-form'>
             <label htmlFor="tweet">
               <img src="http://github.com/rdg-404.png" alt="Rodrigo Paiva" />
-              <textarea id="tweet" placeholder="Tweet your answer"/>
+              <textarea 
+                id="tweet"
+                placeholder="Tweet your answer"
+                value={newAnswer}
+                onChange={(event) => {
+                 setNewAnswer(event.target.value)
+                }}
+                />
             </label>
 
 
@@ -40,7 +58,7 @@ export function Status() {
 
     
 
-        {Answers.map(answer => {
+        {answers.map(answer => {
           return <Tweet key={answer} content={answer}/>
         })}
    
